97982fb6481053c13ba21f2f0df606628c88bf18
Rubai Bian <lnbyk@udel.edu>
2020-11-12 11:45:26 -0500

    add select options

--- src/screen/mainScreen/MainScreen.jsx
+++ src/screen/mainScreen/MainScreen.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { Container, Row, Col, Button, Modal, ListGroup } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  ListGroup,
+  Spinner,
+} from "react-bootstrap";
 import "./aaaa.css";
 import ab from "../../queen.png";
 import x from "../../x.png";
-import { set } from "nprogress";
+import Select from "react-select";
 
 const Item = ListGroup.Item;
 const colors = {
@@ -16,7 +24,9 @@ const colors2 = {
   1: "green",
 };
 
-const visited = new Set();
+// select style
+var options = [{ label: "1 queen", value: 1 }];
+
 export default class MainScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -33,23 +43,46 @@ export default class MainScreen extends React.Component {
       */
       solution: [],
       occupied: new Set(),
+      visited: new Set(),
       queens: 8,
       show: false,
       showselected: false,
       curSelectSolution: null,
+      loading: "none",
     };
   }
 
   componentDidMount() {
+    for (var j = 2; j <= 16; j++) {
+      options.push({ label: `${j} queens`, value: j });
+    }
     var curBoard = [];
     for (var i = 0; i < this.state.queens; i++) {
       curBoard.push(new Array(this.state.queens).fill(0));
       this.state.occupied.add(i);
     }
-    this.setState({ board: curBoard });
-    this.setState({ board2: JSON.parse(JSON.stringify(curBoard)) });
+    this.setState({
+      board: curBoard,
+      board2: JSON.parse(JSON.stringify(curBoard)),
+    });
   }
 
+  selectQueen = (e) => {
+    const val = e.value;
+    var curBoard = [];
+    for (var i = 0; i < val; i++) {
+      curBoard.push(new Array(val).fill(0));
+      this.state.occupied.add(i);
+    }
+    this.setState({
+      board: curBoard,
+      board2: JSON.parse(JSON.stringify(curBoard)),
+      queen: val,
+      visted: new Set(),
+      occupied: new Set(),
+    });
+    this.forceUpdate();
+  };
   clear = () => {
     var newBoard = [];
     this.state.board.forEach((val) => {
@@ -57,11 +90,10 @@ export default class MainScreen extends React.Component {
     });
     this.setState({
       board: newBoard,
-      solution: [],
-      queens: 8,
+      board2: JSON.parse(JSON.stringify(newBoard)),
+      visted: new Set(),
+      occupied: new Set(),
     });
-
-    //alert(JSON.stringify(newBoard));
     this.forceUpdate();
   };
 
@@ -69,7 +101,7 @@ export default class MainScreen extends React.Component {
     const tmpboard = JSON.parse(JSON.stringify(this.state.board));
     this.state.occupied.forEach((val) => {
       for (var i = 0; i < this.state.queens; i++) {
-        if (this.isSave(this.state.board, i, val) === false )
+        if (this.isSave(this.state.board, i, val) === false)
           tmpboard[i][val] = -1;
       }
     });
@@ -91,26 +123,14 @@ export default class MainScreen extends React.Component {
   };
 
   solve = () => {
-    //alert(JSON.stringify(this.state.board));
-    /*
-    var list = [],
-      ans = [];
-      this.backTracking(
-        visited,
-        0,
-        this.state.queens,
-        this.state.board,
-        list,
-        ans
-      );
-    */
-    if (this.solveThePuzzle(this.state.board2, 0) === false)
+    var mysolution = [];
+    if (this.solveThePuzzle(this.state.board2, 0, mysolution) === false)
       alert("there is solution for this board");
 
     const after_filter = [];
-    this.state.solution.forEach((vall) => {
+    mysolution.forEach((vall) => {
       var contains = true;
-      visited.forEach((val) => {
+      this.state.visited.forEach((val) => {
         //alert(val[0] +"" +val[1]);
         if (vall[val[0]][val[1]] === 0) {
           contains = false;
@@ -120,12 +140,15 @@ export default class MainScreen extends React.Component {
     });
 
     //alert(JSON.stringify(after_filter));
-    this.setState({ solution: after_filter });
+    this.setState({ solution: after_filter, loading: "none"});
     this.forceUpdate();
   };
 
   addqueen = (e, i, j) => {
-    if (!this.isSave(this.state.board, i, j) && (this.state.board[i][j] === 0 || this.state.board[i][j] === -1)) {
+    if (
+      !this.isSave(this.state.board, i, j) &&
+      (this.state.board[i][j] === 0 || this.state.board[i][j] === -1)
+    ) {
       alert("this position can not place queen");
       return;
     }
@@ -133,11 +156,11 @@ export default class MainScreen extends React.Component {
     if (curIndex[i][j] === 1) {
       curIndex[i][j] = 0;
       this.setState({ board: curIndex });
-      visited.delete([i, j]);
+      this.state.visited.delete([i, j]);
       this.state.occupied.add(j);
     } else if (curIndex[i][j] === 0 && this.state.queens > 0) {
       curIndex[i][j] = 1;
-      visited.add([i, j]);
+      this.state.visited.add([i, j]);
       this.setState({ board: curIndex });
       this.state.occupied.delete(j);
     } else {
@@ -165,21 +188,21 @@ export default class MainScreen extends React.Component {
     return true;
   };
 
-  solveThePuzzle = (board, col) => {
+  solveThePuzzle = (board, col, mysolution) => {
     if (col === board.length) {
-      this.state.solution.push(JSON.parse(JSON.stringify(board)));
+      mysolution.push(JSON.parse(JSON.stringify(board)));
       return true;
     }
 
     var res = false;
     for (var i = 0; i < board.length; i++) {
-      if (visited.has(col + "" + i + "")) {
+      if (this.state.visited.has(col + "" + i + "")) {
         alert(1);
         continue;
       }
       if (this.isSave(board, i, col)) {
         board[i][col] = 1;
-        res = this.solveThePuzzle(board, col + 1) || res;
+        res = this.solveThePuzzle(board, col + 1, mysolution) || res;
         board[i][col] = 0;
       }
     }
@@ -205,6 +228,13 @@ export default class MainScreen extends React.Component {
     const { show, showselected } = this.state;
     return (
       <>
+        <Spinner
+          animation="border"
+          role="status"
+          style={{ display: `${this.state.loading}` }}
+        >
+          <span className="sr-only">Loading...</span>
+        </Spinner>
         <Modal show={showselected} onHide={this.handleCloseS}>
           <Modal.Header closeButton>
             <Modal.Title>{`Current selected solution`}</Modal.Title>
@@ -251,7 +281,7 @@ export default class MainScreen extends React.Component {
             </Button>
           </Modal.Footer>
         </Modal>
-        <Container style={{ marginTop: "10vh", marginBottom: " 10vh"}}>
+        <Container style={{ marginTop: "10vh", marginBottom: " 10vh" }}>
           <Row>
             <Col xs={3}>
               {" "}
@@ -274,15 +304,16 @@ export default class MainScreen extends React.Component {
               </Button>
             </Col>
             <Col xs={3}>
-              {" "}
-              <Button
-                style={{ width: "80%" }}
-                variant="warning"
-                onClick={this.clear}
-              >
-                clear
-              </Button>
+              <Select
+                onChange={(e) => {
+                  console.log(e);
+                  this.selectQueen(e);
+                }}
+                label="Select Queens"
+                options={options}
+              />
             </Col>
+
             <Col xs={3}>
               {" "}
               <Button
@@ -294,69 +325,75 @@ export default class MainScreen extends React.Component {
               </Button>
             </Col>
           </Row>
-          <div style={{outline: "solid 5px", marginTop:'10px',  marginBottom:'12px'}}>
-          {this.state.board.map((val, index) => {
-            return (
-              <Row
-                xs={10}
-                key={"row" + index}
-                style={{
-                  height: (100 / this.state.board.length) * 0.8 + "vh",
-                  alignContent: "center",
-                  justifyContent: "center",
-                  textAlignVertical: "center",
-                  textAlign: "center",
-                  margin: "0.1vh",
-                }}
-              >
-                {val.map((vall, a) => {
-                  const cur = index % 2 === 0 ? colors2 : colors;
-                  var img = "";
-                  switch (vall) {
-                    default:
-                      img = "";
-                      break;
-                    case 1:
-                      img = ab;
-                      break;
-                    case -1:
-                      img = x;
-                      break;
-                  }
-                  return (
-                    <Col
-                      xs={16 / vall.length}
-                      style={{
-                        alignContent: "center",
-                        justifyContent: "center",
-                        textAlignVertical: "center",
-                        textAlign: "center",
-                        backgroundColor: cur[(a % 2) + 1],
-                        margin: "0.1vh",
-                        height: "100%",
-                      }}
-                    >
-                      <Button
+          <div
+            style={{
+              outline: "solid 5px",
+              marginTop: "10px",
+              marginBottom: "12px",
+            }}
+          >
+            {this.state.board.map((val, index) => {
+              return (
+                <Row
+                  xs={10}
+                  key={"row" + index}
+                  style={{
+                    height: (100 / this.state.board.length) * 0.8 + "vh",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    textAlignVertical: "center",
+                    textAlign: "center",
+                    margin: "0.1vh",
+                  }}
+                >
+                  {val.map((vall, a) => {
+                    const cur = index % 2 === 0 ? colors2 : colors;
+                    var img = "";
+                    switch (vall) {
+                      default:
+                        img = "";
+                        break;
+                      case 1:
+                        img = ab;
+                        break;
+                      case -1:
+                        img = x;
+                        break;
+                    }
+                    return (
+                      <Col
+                        xs={16 / vall.length}
                         style={{
-                          backgroundColor: "transparent",
-                          outlineColor: "transparent",
-                          borderColor: "transparent",
-                          marginTop: "10%",
-                          backgroundImage: `url(${img})`,
-                          backgroundSize: "100% 100%",
-                          height: "80%",
-                          width: "80%",
+                          alignContent: "center",
+                          justifyContent: "center",
+                          textAlignVertical: "center",
+                          textAlign: "center",
+                          backgroundColor: cur[(a % 2) + 1],
+                          margin: "0.1vh",
+                          height: "100%",
                         }}
-                        onClick={(e) => {
-                          this.addqueen(e, index, a);
-                        }}
-                      ></Button>
-                    </Col>
-                  );
-                })}
-              </Row>
-            );
-          })}
+                      >
+                        <Button
+                          style={{
+                            backgroundColor: "transparent",
+                            outlineColor: "transparent",
+                            borderColor: "transparent",
+                            marginTop: "10%",
+                            backgroundImage: `url(${img})`,
+                            backgroundSize: "100% 100%",
+                            height: "80%",
+                            width: "80%",
+                          }}
+                          onClick={(e) => {
+                            this.addqueen(e, index, a);
+                          }}
+                        ></Button>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              );
+            })}
           </div>
           <Row>
             <Col xs={3}>
@@ -367,6 +404,16 @@ export default class MainScreen extends React.Component {
                 onClick={this.unavailable}
               >
                 show unavailable area
+              </Button>
+            </Col>
+            <Col xs={3}>
+              {" "}
+              <Button
+                style={{ width: "80%" }}
+                variant="warning"
+                onClick={this.clear}
+              >
+                clear
               </Button>
             </Col>
           </Row>
